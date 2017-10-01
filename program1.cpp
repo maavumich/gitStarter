1a1f1f7d79f04b57076b132fc56714e197bf91a1
Nicholas Eckardt <neckardt@umich.edu>
2017-10-01 00:00:45 -0400

    added simple files to test git with

---
+++ program1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "hello world" << endl;
+
+	cout << "Man what an interesting program, you really must have spent"
+		" days coming up with this." << endl;
+
+	return 0;
+}
---
+++ program2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "hello program2" << endl;
+
+	cout << "This will not teach you how to download more ram." << endl;
+
+	return 0;
+}
